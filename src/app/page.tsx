510f46eb07540882048e097b36234b62a6ab540f
YouRekt <czarnecki.jurko@gmail.com>
2024-10-23 02:05:41 +0200

    Adjusted timer duration as in example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Timer from "./components/timer";
 export default function Home() {
 	return (
 		<div className="bg-[#444] w-full h-screen flex items-center justify-center">
-			<Timer title={"My Timer"} endTime={3599} elapsedTime={2500} />
+			<Timer title={"My Timer"} endTime={152} elapsedTime={13} />
 		</div>
 	);
 }
